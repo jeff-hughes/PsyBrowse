09788650d47be0e0838d30db5d0a84d9b909768d
Jeff Hughes <jeff.hughes@gmail.com>
2014-10-01 18:10:32 -0400

    Created user profile for auto-generated admin.
    Also, added LOGIN_REDIRECT_URL so login page redirects properly afterward.

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 from django.contrib.auth.models import User
+from psybrowse_app.models import UserProfile
+
 if User.objects.count() == 0:
     admin = User.objects.create(username='jeff.hughes@gmail.com')
     admin.set_password('admin')
+    admin.email = admin.username  # username and email are the same
+    admin.first_name = 'Jeff'
+    admin.last_name = 'Hughes'
     admin.is_superuser = True
     admin.is_staff = True
     admin.save()
+
+    user_profile = UserProfile(user=admin, affiliation='University of Waterloo')
+    user_profile.save()
