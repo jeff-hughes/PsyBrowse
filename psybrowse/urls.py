5c85a0bdb813f68884a88120cef966b36b68571e
Jeff Hughes <jeff.hughes@gmail.com>
2014-10-01 17:50:10 -0400

    Changed STATIC_URL path back to what it was and edited STATIC_ROOT instead. (reverted from commit 0ed15046b246041f1440c84aedd94154a63627b7)
    Also modified URLs to set app into root URL directory.

--- psybrowse/urls.py
+++ psybrowse/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'psybrowse.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('psybrowse_app.urls')),
+    url(r'^', include('psybrowse_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
