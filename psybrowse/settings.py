2762d6bcbfdc609c3aeda9524684b6447013afb1
Jeff Hughes <jeff.hughes@gmail.com>
2014-10-01 17:54:09 -0400

    Changed STATIC_URL path back to what it was before (for realz).

--- psybrowse/settings.py
+++ psybrowse/settings.py
@@ -90,7 +90,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static/')
 
 # Login URL: the login_required() function uses this to redirect; can be a view name as well
 LOGIN_URL = 'django.contrib.auth.views.login'
